b5ce6368b20bc2a84124e6b3a1a43cdc2e577512
nabiloo19 <nabil.alanssi19@gmail.com>
2020-04-12 15:47:41 +0300

    :sparkles: Basic App Structure

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container class="text-center">
+
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+
+    data: () => ({
+    
+    }),
+  }
+</script>
